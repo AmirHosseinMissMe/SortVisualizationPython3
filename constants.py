a0d76d8b746f3813e74e21bff4624158beaf7fc0
AmirHosseinMissMe <amirhossein2324236@gmail.com>
2020-11-29 18:14:58 +0330

    Project init done

---
+++ constants.py
@@ -0,0 +1,34 @@
+import pygame
+
+pygame.font.init()
+ARIAL14 = pygame.font.SysFont('Arial', 14)
+ARIAL12 = pygame.font.SysFont('Arial', 12)
+IMPACT20 = pygame.font.SysFont('Impact', 24)
+
+CIRCLE_POS = [
+    (15, 535),
+    (15, 555),
+    (15, 575),
+    (15, 595),
+    (15, 615),
+
+    (105, 535),
+    (105, 555),
+    (105, 575),
+    (105, 595),
+    (105, 615),
+
+    (200, 535),
+    (200, 555),
+    (200, 575),
+    (200, 595),
+    (200, 615)
+]
+
+BUTTONS_POS = [
+    (400, 530, 100, 50),
+    (600, 530, 100, 50)
+]
+
+WIDTH = 800
+HEIGHT = 630
